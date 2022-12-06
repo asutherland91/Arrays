62c776b00daabe236a5343271456b2ae50179733
Adriane Sutherland <asutherland91@gmail.com>
2022-12-06 14:47:27 -0800

    Add methods and explanations of methods the the arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
 var cats = ["Nyx", "Artemis", "Persephone", "Orion"];
+//pop removes the last element from the array and then returns the array minus that element.
+cats.pop();
 var ages = [10, 9, 2, 2];
+//shift removes the first element from the array and then returns the array minus that element.
+ages.shift();
 var likesWater = [true, false, false, true];
+//unshift will insert the new element added into the parenthesis to the beginning of the array 
+//and then returns the array plus that element.
+likesWater.unshift(false);
+
+console.log(cats);
+console.log(ages);
+console.log(likesWater);
